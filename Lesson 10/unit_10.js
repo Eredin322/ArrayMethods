cd694f2dd4d9466fe7e1a97e620769e7c95652cd
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-11-26 11:38:11 +0200

    HW #10

--- Lesson 10/unit_10.js
+++ Lesson 10/unit_10.js
@@ -5,7 +5,7 @@
 let a1 = [22, 33, 44, 55, 66, 77, 88];
 
 const f1 = () => {
-
+    document.querySelector('.out-1').innerHTML = a1.reverse();
 }
 
 
@@ -15,7 +15,7 @@ const f1 = () => {
 let s2 = 'mazahackercrackall';
 
 const f2 = () => {
-
+    document.querySelector('.out-2').innerHTML = s2.split('').reverse().join('');
 }
 
 // TASK 03
@@ -23,9 +23,13 @@ const f2 = () => {
 
 
 let a3 = [[44, 55, 66], [77, 88, 99]]; // ожидаю 66 55 44 99 88 77
+let a3_res = [];
 
 const f3 = () => {
-
+    for (let i = 0; i < a3.length; i++) {
+        a3_res.push(a3[i].reverse());
+    }
+    document.querySelector('.out-3').innerHTML = a3_res;
 }
 
 // TASK 04
@@ -35,7 +39,10 @@ const f3 = () => {
 let a4 = [55, 66, 77, 88, 99];
 
 const f4 = () => {
-
+    let res = a4.reduce((accum, item) => {
+        return [item, ...accum];
+    }, [])
+    document.querySelector('.out-4').innerHTML = res;
 }
 
 // TASK 05
@@ -44,16 +51,24 @@ const f4 = () => {
 let a5 = [1, 2, 3, 4, 5, 6, 7, 8]; // ожидаю [2, 1, 4, 3, 6, 5, 8, 7]
 
 const f5 = () => {
+    let a5_res = [];
+    for (let i = 0; i < a5.length; i = i + 2) {
+        a5_res.push(a5[i + 1]);
+        a5_res.push(a5[i]);
+    }
+    document.querySelector('.out-5').innerHTML = a5_res;
 }
 
 // TASK 06
 //  Напишите функцию f6, которая запускается по кнопке b-6. Функция сортирует массив a6 по возрастанию и выводит в out-6 через пробел. Не указывайте функции сортировки. Изучите результат на двух значениях переменной a6.
 
 
-let a6 = [1, 4, 2, 6, 7, 3, 5, 2, 9];
-// a6 = ['hi', 'low', 'lo', 'hihi'];
+// let a6 = [1, 4, 2, 6, 7, 3, 5, 2, 9];
+a6 = ['hi', 'low', 'lo', 'hihi'];
 
 const f6 = () => {
+    let a6_res = a6.sort();
+    document.querySelector('.out-6').innerHTML = a6_res;
 }
 
 // TASK 07
@@ -63,6 +78,8 @@ const f6 = () => {
 let a7 = [10, 4, 20, 6, 70, 30, 5, 2, 9];
 
 const f7 = () => {
+    let a7_res = a7.sort((a, b) => a - b)
+    document.querySelector('.out-7').innerHTML = a7_res;
 }
 
 // TASK 08
@@ -71,6 +88,20 @@ const f7 = () => {
 let a8 = [10, 4, 20, 6, 70, 30, 5, 2, 9];
 
 const f8 = () => {
+    let a8_res = a8.slice();
+    for (let k = 0; k < a8_res.length; k++) {
+        for (let i = 0; i < a8_res.length; i++) {
+            let greater;
+            if (a8_res[i] - a8_res[i + 1] > 0) {
+                greater = a8_res[i];
+                a8_res[i] = a8_res[i + 1];
+                a8_res[i + 1] = greater;
+            } else {
+                continue;
+            }
+        }
+    }
+    return a8_res;
 }
 
 // TASK 09
@@ -79,6 +110,14 @@ const f8 = () => {
 let a9 = [9, 9, 45, 3, 1, 83, 50, 6, 5, 35, 8, 4];
 
 const f9 = (s) => {
+    let a9_res;
+    if (s === 'ASC') {
+        a9_res = a9.sort((a, b) => a - b)
+    }
+    if (s === 'DESC') {
+        a9_res = a9.sort((a, b) => b - a)
+    }
+    return a9_res;
 }
 
 // TASK 10
@@ -97,6 +136,13 @@ let a10 = [
 ];
 
 const f10 = () => {
+    let a10_res = a10.sort((a, b) => {
+        return a.age - b.age;
+    })
+    document.querySelector('.out-10').innerHTML = '';
+    for (let i = 0; i < a10_res.length; i++) {
+        document.querySelector('.out-10').innerHTML += a10_res[i].name + ' ';
+    }
 }
 
 
@@ -108,8 +154,10 @@ document.querySelector('.b-4').addEventListener('click', f4);
 document.querySelector('.b-5').addEventListener('click', f5);
 document.querySelector('.b-6').addEventListener('click', f6);
 document.querySelector('.b-7').addEventListener('click', f7);
-document.querySelector('.b-8').addEventListener('click', f8);
+document.querySelector('.b-8').addEventListener('click', () => {
+    document.querySelector('.out-8').innerHTML = f8();
+});
 document.querySelector('.b-9').addEventListener('click', () => {
-    document.querySelector('.out-9').innerHTML = f9('ASC');
+    document.querySelector('.out-9').innerHTML = f9('DESC');
 });
 document.querySelector('.b-10').addEventListener('click', f10);
